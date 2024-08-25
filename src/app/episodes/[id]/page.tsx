c1327e18ded58ab3ac3146cc2d16b5cf7f193959
tokiya.horikawa <ayikot4423@gmail.com>
2024-08-26 08:38:00 +0900

    feat: ページにmeta情報を追加

--- src/app/episodes/[id]/page.tsx
+++ src/app/episodes/[id]/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import EpisodeDetail from "../_components/EpisodeDetail";
 import type { PodcastEpisode } from "@/lib/types";
 import { generateMockEpisode } from "@/lib/mock";
+
+export const metadata: Metadata = {
+  title: "エピソード詳細 - Podcast Hub",
+  description: "Podcastに貢献できる場所",
+};
 
 const EpisodePage = ({ params }: { params: { id: string } }) => {
   const episode: PodcastEpisode = getEpisode(params.id);
