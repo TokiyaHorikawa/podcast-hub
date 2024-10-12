a5104a02d27ac43581a1bafe7fb3590c443e9171
tokiya.horikawa <ayikot4423@gmail.com>
2024-10-12 23:19:01 +0900

    feat: ログイン画面の作成

---
+++ src/app/login/login.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import useUser from "./useUser";
+import useSignInWithPassword from "./useSignInWithPassword";
+
+export default function Login() {
+  // TODO: Form Library使う？
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // NOTE: ログインしているかどうかをチェック
+  useUser();
+  const { signInWithPassword, error } = useSignInWithPassword();
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // NOTE: これは全体にデフォルトの設定にした方がいい？
+    await signInWithPassword({ email, password });
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>ログイン</CardTitle>
+          <CardDescription>アカウントにログインしてください</CardDescription>
+        </CardHeader>
+        <form onSubmit={handleLogin}>
+          <CardContent>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">メールアドレス</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="your@email.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">パスワード</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <Button className="w-full" type="submit">
+              ログイン
+            </Button>
+            {error && <p className="text-red-500 mt-2">{error}</p>}
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+}
---
+++ src/app/login/useSignInWithPassword.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
+import supabase from "@/lib/supabase/supabase";
+import { useState } from "react";
+
+export default function useSignInWithPassword() {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  const signInWithPassword = async ({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) => {
+    setError(null);
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) throw error;
+
+      router.push("/");
+    } catch (error) {
+      setError(
+        "ログインに失敗しました。メールアドレスとパスワードを確認してください。"
+      );
+      return { error };
+    }
+  };
+
+  return { signInWithPassword, error };
+}
