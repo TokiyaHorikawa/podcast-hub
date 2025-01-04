dc37f246c654a7b69c22bff560d450482460b465
tokiya.horikawa <ayikot4423@gmail.com>
2025-01-04 21:02:09 +0900

    chore: バリデーションを追加

--- src/app/posts/new/_components/NewPostForm.tsx
+++ src/app/posts/new/_components/NewPostForm.tsx
@@ -4,16 +4,30 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import * as z from "zod";
 import RichEditor from "./RichEditor";
 
-type FormValues = {
-  title: string;
-  content: string;
-};
+const postSchema = z.object({
+  title: z
+    .string()
+    .min(1, "タイトルは必須です")
+    .max(50, "タイトルは50文字以内で入力してください"),
+  content: z.string().max(2000, "本文は2000文字以内で入力してください"),
+});
+
+type FormValues = z.infer<typeof postSchema>;
 
 export default function NewPostForm() {
-  const { register, handleSubmit, setValue, watch } = useForm<FormValues>({
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(postSchema),
     defaultValues: {
       title: "",
       content: "",
@@ -33,8 +47,11 @@ export default function NewPostForm() {
           id="title"
           placeholder="投稿のタイトルを入力してください"
           className="text-lg"
-          {...register("title", { required: true })}
+          {...register("title")}
         />
+        {errors.title && (
+          <p className="text-sm text-red-500">{errors.title.message}</p>
+        )}
       </div>
 
       <div className="space-y-2">
@@ -45,6 +62,9 @@ export default function NewPostForm() {
             onChange={(value: string) => setValue("content", value)}
           />
         </Card>
+        {errors.content && (
+          <p className="text-sm text-red-500">{errors.content.message}</p>
+        )}
       </div>
 
       <div className="flex justify-between">
