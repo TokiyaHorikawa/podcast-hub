df53616ab4f23cd676736bc816671cac2c8ef96e
tokiya.horikawa <ayikot4423@gmail.com>
2025-01-05 18:31:25 +0900

    fix: typecheck

--- src/lib/mock.ts
+++ src/lib/mock.ts
@@ -1,21 +1,15 @@
-import type { Channel, Content, Episode, User } from "@/lib/types";
-
-export function generateMockContent(id: bigint): Content {
-  return {
-    id,
-    title: `タイトル ${id}`,
-    body: `コンテンツ ${id}の詳細な説明です。このコンテンツは、さまざまな情報を提供し、読者にとって有益な内容を含んでいます。さらに、関連するトピックや事例を交えながら、深く掘り下げていきます。最終的には、読者がこの情報を活用できるように、具体的なアクションプランや提案も含めています。`,
-  };
-}
+import type { Channel, Episode } from "@/lib/types";
 
 export function generateMockEpisode(id: string): Episode {
   return {
-    id,
+    id: Number(id),
     title: `エピソード ${id} のタイトル`,
     description: `これはエピソード ${id} の詳細な説明です。このポッドキャストエピソードでは、興味深いトピックについて議論し、リスナーに価値ある情報を提供します。`,
     imageUrl:
       "https://i.scdn.co/image/ab67656300005f1fb412cc05140e5eedc61ac87d",
     channel: generateMockChannel("1"),
+    podcastId: Number(id),
+    createdAt: new Date(),
   };
 }
 
